3858b2c9763bb7e02d49c6d8d829d892bdcd850b
Munavvarxon <lider_prince@mail.ru>
2022-08-03 05:44:52 +0500

    Add new files

---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+
+import wikipedia
+from aiogram import Bot, Dispatcher, executor, types
+wikipedia.set_lang('uz')
+
+API_TOKEN = '5487420195:AAEQgV6-CE-vTdhfalZDsumR8N7gz_V-9fw'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Assalammmu alaykum!\n Wikipedia botiga xush kelibsiz")
+
+@dp.message_handler()
+async def sendWiki(message: types.Message):
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer('Bu mavzuga oid maqola topilmadi')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
